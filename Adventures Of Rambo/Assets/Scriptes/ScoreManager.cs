6f2ace256d6258ae2606665fded10b109bf7189b
Syed-Shameer <116372047+Syed-Shameer@users.noreply.github.com>
2024-09-05 02:52:17 +0500

    scoring setup

---
+++ Adventures Of Rambo/Assets/Scriptes/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public int score = 0;
+    public Text scoreText;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreUI();
+    }
+
+    private void UpdateScoreUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
+    }
+}
